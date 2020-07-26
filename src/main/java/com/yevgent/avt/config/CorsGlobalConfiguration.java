7c16ac0ae2afd4f8161ed44cda7f5fc8490f3333
Yevgen Taran <yevgenii.taran@gmail.com>
2020-07-26 21:23:20 +0300

    cors

---
+++ src/main/java/com/yevgent/avt/config/CorsGlobalConfiguration.java
@@ -0,0 +1,32 @@
+package com.yevgent.avt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+    private static final String ALLOWED_HEADERS = "*";
+    private static final String ALLOWED_METHODS = "*";
+    private static final String ALLOWED_ORIGIN = "*";
+    private static final String MAX_AGE = "3600";
+
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+                headers.add("Access-Control-Max-Age", MAX_AGE);
+                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
+            return chain.filter(ctx);
+        };
+    }
+
+}
